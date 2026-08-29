17659cf242680d74a5904b72792a42c8786842e4
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-08-29 14:51:52 +0700

    feat: expose live equipment phase3 preview

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AppErrorBoundary } from './AppErrorBoundary'
+import { LiveEquipmentPanel } from './LiveEquipmentPanel'
+
+const phase3Preview = new URLSearchParams(window.location.search).get('phase3')
+const content = phase3Preview === 'equipment'
+  ? <div className="app-body"><main id="main-content" className="main-content"><LiveEquipmentPanel /></main></div>
+  : <App />
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppErrorBoundary>
-      <App />
+      {content}
     </AppErrorBoundary>
   </StrictMode>,
 )
