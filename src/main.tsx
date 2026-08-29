28a2a5806d84589704d414c28b702bdb2ea1f9bf
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-08-29 15:10:39 +0700

    feat: add calibration live preview route

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AppErrorBoundary } from './AppErrorBoundary'
+import { LiveCalibrationPanel } from './LiveCalibrationPanel'
 import { LiveEquipmentPanel } from './LiveEquipmentPanel'
 
 const phase3Preview = new URLSearchParams(window.location.search).get('phase3')
 const content = phase3Preview === 'equipment'
   ? <div className="app-body"><main id="main-content" className="main-content"><LiveEquipmentPanel /></main></div>
-  : <App />
+  : phase3Preview === 'calibration'
+    ? <div className="app-body"><main id="main-content" className="main-content"><LiveCalibrationPanel /></main></div>
+    : <App />
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
