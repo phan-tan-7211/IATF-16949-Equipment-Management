7c30f55b9c6431b399331a57a44c98aa548f9322
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-09-05 16:46:20 +0700

    refactor: add desktop equipment workspace entry

---
+++ src/equipment/desktop/EquipmentDesktopWorkspace.tsx
@@ -0,0 +1,8 @@
+import '../../equipment/desktop/EquipmentDesktop.css'
+import { LiveEquipmentRegistrationPanel } from '../../LiveEquipmentRegistrationPanel'
+import { LiveEquipmentPanel } from '../../LiveEquipmentPanel'
+
+/** Desktop Equipment composition. Mobile composition must not be added here. */
+export function EquipmentDesktopWorkspace() {
+  return <div className="maintenance-workspace-stack equipment-desktop-workspace"><LiveEquipmentRegistrationPanel/><LiveEquipmentPanel/></div>
+}
